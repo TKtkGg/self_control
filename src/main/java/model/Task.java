7a5b10bf15f1bcac4d4271ec9c0f4fc5b16560f6
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-02 15:32:11 +0900

    タスクモデル作成

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.time.LocalTime;
+
+public class Task {
+	private int id;
+	private int scheduleId;
+	private LocalTime startTime;
+	private LocalTime endTime;
+	private String taskName;
+	private String memo;
+	
+	public Task(int id, int scheduleId, LocalTime startTime, LocalTime endTime, String taskName, String memo) {
+		this.id = id;
+		this.scheduleId = scheduleId;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.taskName = taskName;
+		this.memo = memo;
+	}
+	
+	public int getId() {
+		return this.id;	
+	}
+	
+	public int getScheduleId() {
+		return this.scheduleId;
+	}
+	
+	public LocalTime getStartTime() {
+		return this.startTime;
+	}
+	
+	public LocalTime getEndTime() {
+		return this.endTime;
+	}
+	
+	public String getTaskName() {
+		return this.taskName;
+	}
+	
+	public String getMemo() {
+		return this.memo;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setScheduleId(int scheduleId) {
+		this.scheduleId = scheduleId;
+	}
+	
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+	
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+	
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+}
+

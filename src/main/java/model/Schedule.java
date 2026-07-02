52b70b8d2d30e6ef87d54eb73ee3665168d321f3
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-02 14:15:01 +0900

    スケジュールモデルの作成

---
+++ src/main/java/model/Schedule.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Schedule {
+	private int id;
+	private int userId;
+	private int dayOfWeek;
+	private String title;
+	
+	public Schedule(int id, int userId, int dayOfWeek, String title) {
+		this.id = id;
+		this.userId = userId;
+		this.dayOfWeek = dayOfWeek;
+		this.title = title;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public int getUserId() {
+		return this.userId;
+	}
+	
+	public int getDayOfWeek() {
+		return this.dayOfWeek;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public void setDayOfWeek(int dayOfWeek) {
+		this.dayOfWeek = dayOfWeek;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}

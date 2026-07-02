3317f2ba8bf82cd29bdd929112cdbe130ca4e7c3
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-02 13:24:48 +0900

    データベース接続のmodel作成

---
+++ src/main/java/model/DBConnection.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		final String URL = "jdbc:postgresql://localhost:5432/self_controldb";
+		final String USER = "miyamotoyuuki";
+		final String PASSWORD = "postgres";
+		
+		Class.forName("org.postgresql.Driver");
+		Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+		return con;
+	}
+}

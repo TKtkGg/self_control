92bfdcfd18fb1b8b8a862142573256fb4cbc20e1
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-04 15:12:08 +0900

    likeDAOの作成

--- src/main/java/com/tktkgg/self_control/dao/LikeDao.java
+++ src/main/java/com/tktkgg/self_control/dao/LikeDao.java
@@ -1,5 +1,60 @@
 package com.tktkgg.self_control.dao;
 
-public class LikeDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.tktkgg.self_control.model.Like;
+import com.tktkgg.self_control.util.DBConnection;
+
+public class LikeDao {
+	public List<Like> findByScheduleId(int scheduleId) throws  ClassNotFoundException, SQLException {
+		List<Like> likeList = new ArrayList<Like>();
+		
+		String sql = "SELECT * FROM likes WHERE schedule_id = ?";
+		
+		try(Connection con = DBConnection.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql)) {
+			
+			pstmt.setInt(1, scheduleId);
+			
+			try (ResultSet rs = pstmt.executeQuery()) {
+				while(rs.next()) {
+					likeList.add(
+						new Like(
+							rs.getInt("id"),
+							rs.getInt("user_id"),
+							rs.getInt("schedule_id")
+							
+						)
+					);
+				}
+				
+				return likeList;
+			}
+		}
+	}
+	
+	public int countByScheduleId(int scheduleId) throws  ClassNotFoundException, SQLException {
+		int likeCount = 0;
+		
+		String sql = "SELECT COUNT(*) AS like_count FROM likes WHERE schedule_id = ?";
+		
+		try(Connection con = DBConnection.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql)) {
+			
+			pstmt.setInt(1, scheduleId);
+			
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
+					return rs.getInt("like_count");
+				}
+				
+				return likeCount;
+			}
+		}
+	}
 }

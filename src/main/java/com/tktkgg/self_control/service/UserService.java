addc5c36e9ca18ebb740de113202854e7d59f2cc
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-11 02:10:51 +0900

    UserService作成

--- src/main/java/com/tktkgg/self_control/service/UserService.java
+++ src/main/java/com/tktkgg/self_control/service/UserService.java
@@ -1,5 +1,28 @@
 package com.tktkgg.self_control.service;
 
-public class UserService {
+import java.sql.SQLException;
+import java.util.List;
 
+import com.tktkgg.self_control.dao.UserDao;
+import com.tktkgg.self_control.model.User;
+import com.tktkgg.self_control.util.SessionManager;
+
+public class UserService {
+	private final UserDao ud = new UserDao();
+	
+	public List<User> getUsers() throws ClassNotFoundException, SQLException {
+		return ud.findAll();
+	}
+	
+	public User getUser(int id) throws ClassNotFoundException, SQLException {
+		return ud.findById(id);
+	}
+	
+	public void updateUser(User user) throws ClassNotFoundException, SQLException {
+		ud.update(user);
+	}
+	
+	public User getCurrentUser(){
+		return SessionManager.getUser();
+	}
 }

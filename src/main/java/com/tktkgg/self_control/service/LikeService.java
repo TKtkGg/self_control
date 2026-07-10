016f3a2eac6a2594e0a91c45e991395731d91035
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-11 01:52:56 +0900

    LikeService作成

--- src/main/java/com/tktkgg/self_control/service/LikeService.java
+++ src/main/java/com/tktkgg/self_control/service/LikeService.java
@@ -1,5 +1,37 @@
 package com.tktkgg.self_control.service;
 
-public class LikeService {
+import java.sql.SQLException;
 
+import com.tktkgg.self_control.dao.LikeDao;
+import com.tktkgg.self_control.model.Like;
+import com.tktkgg.self_control.util.SessionManager;
+
+public class LikeService {
+	private final LikeDao ld = new LikeDao();
+	
+	public boolean isLiked(int userId, int scheduleId) throws ClassNotFoundException, SQLException {
+		return ld.exists(userId, scheduleId);
+	}
+	
+	public int countLikes(int scheduleId) throws ClassNotFoundException, SQLException {
+		return ld.countByScheduleId(scheduleId);
+	}
+	
+	public void like(int scheduleId) throws ClassNotFoundException, SQLException {
+		int userId = SessionManager.getUser().getId();
+		Like like = new Like(0, userId, scheduleId);
+		
+		if (!isLiked(like.getUserId(), like.getScheduleId())) {
+			ld.create(like);
+		}
+	}
+	
+	public void unlike(int scheduleId) throws ClassNotFoundException, SQLException {
+		int userId = SessionManager.getUser().getId();
+		Like like = new Like(0, userId, scheduleId);
+		
+		if (isLiked(like.getUserId(), like.getScheduleId())) {
+			ld.delete(like.getUserId(), like.getScheduleId());
+		}
+	}
 }

e04091eef700147df4a6cf10e4cc8d414be5bacb
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-18 13:58:08 +0900

    いいね！機能追加

--- src/main/java/com/tktkgg/self_control/view/UserView.java
+++ src/main/java/com/tktkgg/self_control/view/UserView.java
@@ -9,17 +9,20 @@ import java.util.Locale;
 import com.tktkgg.self_control.model.Schedule;
 import com.tktkgg.self_control.model.Task;
 import com.tktkgg.self_control.model.User;
+import com.tktkgg.self_control.service.LikeService;
 import com.tktkgg.self_control.service.ScheduleService;
 import com.tktkgg.self_control.service.TaskService;
 import com.tktkgg.self_control.service.UserService;
 import com.tktkgg.self_control.util.Input;
 import com.tktkgg.self_control.util.InputUtils;
 import com.tktkgg.self_control.util.ScheduleUtils;
+import com.tktkgg.self_control.util.SessionManager;
 
 public class UserView {
 	UserService us = new UserService();
 	ScheduleService ss = new ScheduleService();
 	TaskService ts = new TaskService();
+	LikeService ls = new LikeService();
 	
 	public void usersView() throws ClassNotFoundException, SQLException {
 		List<User> users = us.getUsers();
@@ -56,12 +59,12 @@ public class UserView {
 		while (true) {
 			System.out.println("どのスケジュールを確認しますか（1:月 2:火 3:水 4:木 5:金 6:土 7:日）（0で戻る）");
 			int i = InputUtils.inputWeek();
-			if (i == 0) return;
+			if (i == 0) break;
 			
 			DayOfWeek day = DayOfWeek.of(i);
 			
 			Schedule schedule = ss.getSpecificSchedule(user, day);
-			if (ScheduleUtils.isScheduleNull(schedule)) return;
+			if (ScheduleUtils.isScheduleNull(schedule)) break;
 			
 			List<Task> tasks = ts.getTasks(schedule.getId());
 		
@@ -75,9 +78,27 @@ public class UserView {
 				System.out.println(task.getMemo());
 				System.out.println();
 			}
+			System.out.println("いいね！：" + ls.countLikes(schedule.getId()));
 			
 			System.out.println();
+			
+			if (!ls.isLiked(SessionManager.getUser().getId(), schedule.getId())) {
+				System.out.println("このユーザーのスケジュールに「いいね！」をしますか？（1:はい 2:いいえ）");
+				while (true) {
+					int isLike = Input.nextInt();
+					if (isLike == 1) {
+						ls.like(schedule.getId());
+						System.out.println("「いいね！」しました");
+						break;
+					} else if (isLike == 2) {
+						break;
+					} else {
+						System.out.println("1か2を入力してください。");
+					}
+				}
+			}
 		}
+		
 		
 	}
 }

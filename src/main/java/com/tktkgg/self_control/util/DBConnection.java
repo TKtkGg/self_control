d4487af22d1b54e495d454b7eda3581ec60da4c0
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-07-22 18:27:45 +0900

    DBConnectionの整理

--- src/main/java/com/tktkgg/self_control/util/DBConnection.java
+++ src/main/java/com/tktkgg/self_control/util/DBConnection.java
@@ -5,13 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+	public static Connection getConnection() throws SQLException {
 		final String URL = "jdbc:postgresql://localhost:5432/self_control";
 		final String USER = "self_control_user";
 		final String PASSWORD = "postgres";
 		
-		Class.forName("org.postgresql.Driver");
-		Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
-		return con;
+		return DriverManager.getConnection(URL, USER, PASSWORD);
 	}
 }

4945588814c6714f1d0f6c2f22e8b26f06587e78
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-08-10 00:35:09 +0900

    first commit

---
+++ backend/selfcontrol/src/main/java/com/tktkgg/selfcontrol/SelfcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.tktkgg.selfcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfcontrolApplication.class, args);
+	}
+
+}
---
+++ backend/selfcontrol/src/test/java/com/tktkgg/selfcontrol/SelfcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tktkgg.selfcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
